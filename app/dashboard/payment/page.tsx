e057459031d55f33ec20461b64bdc3a3d975f595
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2025-01-10 22:17:44 -0400

    fix: update some details

--- app/dashboard/payment/page.tsx
+++ app/dashboard/payment/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -9,7 +9,7 @@ import CreditCardInfo from "@/components/dashboard/CreditCardInfo";
 import { FaCcMastercard, FaCcPaypal, FaCcApplePay } from "react-icons/fa";
 
 const PaymentForm = () => {
-  const [selectedPayment, setSelectedPayment] = useState("creditCard"); 
+  const [selectedPayment, setSelectedPayment] = useState("creditCard");
 
   const {
     register,
@@ -20,50 +20,57 @@ const PaymentForm = () => {
     mode: "onBlur",
   });
 
-  const onSubmit = (data:any) => {
+  const onSubmit = (data: any) => {
     console.log("Payment Data:", data);
     console.log("Selected Payment Method:", selectedPayment);
   };
 
   return (
     <div className="max-w-7xl mx-auto p-8">
-      <div className="mb-8 text-center">
-        <h1 className="text-4xl font-extrabold text-gray-800 dark:text-gray-100">Billing Information</h1>
-        <p className="text-gray-500 dark:text-gray-400 mt-2">
-          Choose a payment option below and fill out the appropriate information.
-        </p>
+      <div className="flex justify-between items-center mb-8">
+        <div>
+          <h1 className="text-4xl font-extrabold text-gray-800 dark:text-gray-100">
+            Billing Information
+          </h1>
+          <p className="text-gray-500 dark:text-gray-400 mt-2">
+            Choose a payment option below and fill out the appropriate information.
+          </p>
+        </div>
+        <button
+          type="submit"
+          form="paymentForm"
+          className="px-8 py-4 bg-blue-600 dark:bg-blue-700 text-white text-lg font-medium rounded-lg shadow-md hover:bg-blue-700 dark:hover:bg-blue-800 transition"
+        >
+          Continue
+        </button>
       </div>
 
-      {/* Métodos de pago */}
-      <div className="flex justify-center gap-4 mb-8">
+      <div className="flex  gap-4 mb-8">
         <button
-          className={`flex items-center gap-2 px-6 py-3 border rounded-lg text-lg font-medium transition ${
-            selectedPayment === "creditCard"
+          className={`flex items-center gap-2 px-6 py-3 border rounded-lg text-lg font-medium transition ${selectedPayment === "creditCard"
               ? "bg-blue-600 text-white border-blue-600"
               : "text-blue-600 border-blue-600 hover:bg-blue-600 hover:text-white"
-          }`}
+            }`}
           onClick={() => setSelectedPayment("creditCard")}
         >
           <FaCcMastercard size={24} />
           Credit Card
         </button>
         <button
-          className={`flex items-center gap-2 px-6 py-3 border rounded-lg text-lg font-medium transition ${
-            selectedPayment === "applePay"
+          className={`flex items-center gap-2 px-6 py-3 border rounded-lg text-lg font-medium transition ${selectedPayment === "applePay"
               ? "bg-gray-300 text-gray-800 border-gray-300"
               : "text-gray-600 border-gray-300 hover:bg-gray-300"
-          }`}
+            }`}
           onClick={() => setSelectedPayment("applePay")}
         >
           <FaCcApplePay size={24} />
           Apple Pay
         </button>
         <button
-          className={`flex items-center gap-2 px-6 py-3 border rounded-lg text-lg font-medium transition ${
-            selectedPayment === "paypal"
+          className={`flex items-center gap-2 px-6 py-3 border rounded-lg text-lg font-medium transition ${selectedPayment === "paypal"
               ? "bg-blue-600 text-white border-blue-600"
               : "text-blue-600 border-blue-600 hover:bg-blue-600 hover:text-white"
-          }`}
+            }`}
           onClick={() => setSelectedPayment("paypal")}
         >
           <FaCcPaypal size={24} />
@@ -71,19 +78,16 @@ const PaymentForm = () => {
         </button>
       </div>
 
-      <form className="grid grid-cols-1 lg:grid-cols-2 gap-8" onSubmit={handleSubmit(onSubmit)}>
+      <form
+        id="paymentForm"
+        className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <BillingAddress register={register} errors={errors} touchedFields={touchedFields} />
         <CreditCardInfo register={register} errors={errors} touchedFields={touchedFields} />
-        <div className="mt-10 text-center lg:col-span-2">
-          <button
-            type="submit"
-            className="px-8 py-4 bg-blue-600 dark:bg-blue-700 text-white text-lg font-medium rounded-lg shadow-md hover:bg-blue-700 dark:hover:bg-blue-800 transition"
-          >
-            Continue
-          </button>
-        </div>
       </form>
     </div>
+
   );
 };
 
--- components/auth/SignInForm.tsx
+++ components/auth/SignInForm.tsx
@@ -18,21 +18,21 @@ const SignInForm = ({ onSubmit }: SignInFormProps) => {
     formState: { errors },
   } = useForm<SignInFormData>({
     resolver: yupResolver(signInSchema),
+    mode: "onBlur",
   });
 
   return (
     <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-           Email
+          Email
         </label>
         <input
           type="email"
           id="email"
           {...register("email")}
-          className={`bg-gray-50 border text-gray-900 rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 ${
-            errors.email ? "border-red-500" : "border-gray-300"
-          }`}
+          className={`bg-gray-50 border text-gray-900 rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 ${errors.email ? "border-red-500" : "border-gray-300"
+            }`}
           placeholder="name@company.com"
         />
         {errors.email && <p className="text-red-500 text-sm">{errors.email.message}</p>}
@@ -45,9 +45,8 @@ const SignInForm = ({ onSubmit }: SignInFormProps) => {
           type="password"
           id="password"
           {...register("password")}
-          className={`bg-gray-50 border text-gray-900 rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 ${
-            errors.password ? "border-red-500" : "border-gray-300"
-          }`}
+          className={`bg-gray-50 border text-gray-900 rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 ${errors.password ? "border-red-500" : "border-gray-300"
+            }`}
           placeholder="••••••••"
         />
         {errors.password && <p className="text-red-500 text-sm">{errors.password.message}</p>}
